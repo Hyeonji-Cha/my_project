ad8298606397cc3d901dd65d82a0007542ff6962
Hyeonji-Cha <viphj98@naver.com>
2026-07-23 13:14:19 +0900

    Feat: 로그인 페이지 추가

--- main.py
+++ main.py
@@ -1,18 +1,38 @@
-# 간단한 홈페이지 제공
-# 1. 모듈 가져오기
+from pathlib import Path
+
 from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from fastapi.staticfiles import StaticFiles #정적 데이터 처리 모듈
 
-# 2. Fastapi객체 생성, 전역 변수 생성
+
+BASE_DIR = Path(__file__).resolve().parent
+
 app = FastAPI()
-templates = Jinja2Templates(directory="templates") #정적 데이터 경로 설정
 
-# 3. 라우팅 구성/정의
+app.mount(
+    "/static",
+    StaticFiles(directory=BASE_DIR / "static"),
+    name="static",
+)
+
+templates = Jinja2Templates(
+    directory=BASE_DIR / "templates"
+)
+
+
 @app.get("/")
-def home(req:Request):
-    # 홈페이지( "/")get 방식으로 요청> 매칭되는 함수 home()호출됨
-    # index를 읽어서 req 데이터를 전달하여 동적 html구성
-    # 응답(return)> 클라이언트 브라우저에게 전달 -> 렌더링, Dom tree
-    # 브라우저 해석 화면에 그리기 -> 클라이언트는 응답결과를 화면서 볼수있다
-    return templates.TemplateResponse(req,"index.html")
+def home(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="index.html",
+        context={},
+    )
+
+
+@app.get("/auth/login")
+def home(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="login.html",
+        context={},
+    )
